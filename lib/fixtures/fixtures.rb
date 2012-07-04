0f60e7765b7c8f95280f8ea7046ba175a31a10f6
nm <n.maisonneuve@gmail.com>
2012-07-04 17:08:19 +0300

    enable to the dir  + create it if it does not exist

--- lib/fixtures/fixtures.rb
+++ lib/fixtures/fixtures.rb
@@ -2,7 +2,7 @@ class ActiveRecord::Base
 
 #require 'yaml'
 require 'digest/md5'
-
+require 'fileutils'
 
 def to_hash
   hash = {}; self.attributes.each { |k,v| hash[k.to_sym] = v }
@@ -12,15 +12,18 @@ end
 def to_fixtures
   hash = self.to_hash
   md5 = Digest::MD5.hexdigest(hash.to_s)
-  string = "#{md5}:\n"
+  table_name = self.class.table_name
+  id= "#{table_name}:\n" 
+  string = 
   hash.each do |key,value|
     value.gsub!("\n","\n"+" "*4) if value.class == String
     string += " "*2 + "#{key.to_s}: #{value}\n" unless value.nil?
   end
   string += "\n"
-  table_name = self.class.table_name
+  path=FixtureGenerator.current_dir
+  FileUtils.mkpath path unless File.directory? path
   file_name = table_name + ".yml"
-  File.open("test/fixtures/#{file_name}", 'a') do |out|
+  File.open("#{path}/#{file_name}", 'a') do |out|
     out.write(string)
   end
 end
